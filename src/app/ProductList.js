9dbf28788ad9dfc41a24fb1c588e0465e9bd0791
Rajashekar Talapaka <Rajashekar.Talapaka.TCS@beamsuntory.com>
2024-05-17 11:20:08 +0530

    key added

--- src/app/ProductList.js
+++ src/app/ProductList.js
@@ -19,19 +19,19 @@ function ProductList() {
     <div className='container'>
         <div className='row'>
         {products.map(product => (
-        <div className='col-4 mb-4'>
-       
-          <div key={product.id} class="card">
+        <div key={product.id} className='col-4 mb-4'>       
+          <div className="card">
           {/* <Image src={product.image} width="300" height="500"  /> */}
-          <div class="card-body">
-            <p class="card-text fw-bold">{product.title}</p>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <h5 class="card-title">${product.price}</h5>
-            <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
-            <button onClick={() => dispatch(removeProduct(product.id))}>Remove</button>
+          <div className="card-body">
+            <p className="card-text fw-bold">{product.title}</p>
+            <p className="card-text">
+              {product.description}
+            </p>
+            <h5 className="card-title">${product.price}</h5>
+            <button className='btn btn-primary' onClick={() => handleAddToCart(product)}>Add to Cart</button>
+            <button className='btn btn-danger' onClick={() => dispatch(removeProduct(product.id))}>Remove</button>
           </div>
-        </div>
-       
+        </div>       
         </div>
          ))}
         </div>
